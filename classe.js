bbba02966a9a39f25e4725d94c8c0aedfde10540
WEVERTHON VAZ <112637629+Chanobrair@users.noreply.github.com>
2023-11-16 11:23:45 -0300

    Add files via upload

---
+++ classe.js
@@ -0,0 +1,38 @@
+// Definição da classe Heroi
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque;
+
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'usou magia';
+                break;
+            case 'guerreiro':
+                ataque = 'usou espada';
+                break;
+            case 'monge':
+                ataque = 'usou artes marciais';
+                break;
+            case 'ninja':
+                ataque = 'usou shuriken';
+                break;
+            default:
+                ataque = 'usou um ataque indefinido';
+        }
+
+        console.log(`o ${this.tipo} atacou usando ${ataque}`);
+    }
+}
+
+// Exemplo de uso
+const heroi1 = new Heroi('Aragorn', 35, 'guerreiro');
+const heroi2 = new Heroi('Gandalf', 150, 'mago');
+
+heroi1.atacar(); // Exemplo de saída: o guerreiro atacou usando espada
+heroi2.atacar(); // Exemplo de saída: o mago atacou usando magia
